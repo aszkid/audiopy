16738011fc823609883bb94f7b50d2dd68484875
aszkid <polgomezriquelme@gmail.com>
2014-08-02 17:28:55 +0200

    initial commit

---
+++ test_suite.py
@@ -0,0 +1,28 @@
+"""This file is part of AudioPy.
+
+AudioPy is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+AudioPy is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with AudioPy.  If not, see <http://www.gnu.org/licenses/>."""
+
+import unittest as ut
+
+import logging
+logging.basicConfig()
+log = logging.getLogger("aplog")
+log.setLevel(logging.DEBUG)
+
+import tests.file_read
+
+suite = ut.TestLoader()
+suite = suite.loadTestsFromModule(tests.file_read)
+
+ut.TextTestRunner().run(suite)
---
+++ tests/file_read.py
@@ -0,0 +1,30 @@
+"""This file is part of AudioPy.
+
+AudioPy is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+AudioPy is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with AudioPy.  If not, see <http://www.gnu.org/licenses/>."""
+
+import unittest as ut
+
+import audiopy as ap
+
+class read_various(ut.TestCase):
+ 
+    def setUp(self):
+        pass
+ 
+    def test1(self):
+		mybuff = ap.buffer()
+		self.assertRaises(IOError, mybuff.read_file, "tests/fakefile.wav")
+
+if __name__ == "__main__":
+	ut.main()
