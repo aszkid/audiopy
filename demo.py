46fe75e3631d9a2f8e597aaa6e85d925f22c66c0
aszkid <polgomezriquelme@gmail.com>
2014-08-03 20:18:33 +0200

    - Cleaned demo

--- demo.py
+++ demo.py
@@ -5,14 +5,4 @@ import audiopy as ap
 
 buff = ap.buffer()
 buff.read_file("tests/full_bach.wav")
-
-import pylab as p
-import time
-
-t1 = buff.frame_count(0)
-t2 = buff.frame_count(10)
-p.plot(buff.data[t1:t2, 0])
-p.plot(buff.data[t1:t2, 1])
-p.savefig("{0}.png".format(time.time()))
-
 buff.write_file("tests/full_bach_re.wav")
