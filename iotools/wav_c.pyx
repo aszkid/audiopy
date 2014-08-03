c6ffc31d762a2a54d2bf0e697901de6a20a29f8b
aszkid <polgomezriquelme@gmail.com>
2014-08-03 18:00:06 +0200

    - Optimizing WAV write

--- iotools/wav_c.pyx
+++ iotools/wav_c.pyx
@@ -1,8 +1,10 @@
 import struct
 
-def _populate_chunk(fbeg, fend, indexbeg, chbeg, chend, prop, enc, data, chunk):
-    chind = (chend-chbeg)
-    for frame_i in xrange(fbeg, fend):
+def _populate_chunk(int fbeg, int fend, int indexbeg, int chbeg, int chend, prop, enc, data, chunk):
+    cdef int chind = (chend-chbeg)
+    cdef int i, fcind
+    for frame_i from fbeg <= frame_i < fend:
         i = indexbeg + frame_i
-        for chann_i in xrange(chbeg, chend):
-            chunk[frame_i*chind + chann_i] = struct.pack(enc, data[i][chann_i])
+        fcind = frame_i*chind
+        for chann_i from chbeg <= chann_i < chend:
+            chunk[fcind + chann_i] = struct.pack(enc, data[i][chann_i])
